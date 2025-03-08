bfcb8f71081deb7b105ba0eef11f1f7d0de1bb6d
Celeste Cruz <celesteglezcruz@gmail.com>
2025-03-08 16:55:45 -0500

    añadiendo cambios

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,14 +4,14 @@ export default function Navbar () {
         <header className="bg-white shadow backdrop:blur px-12 py-4 sticky top-0 z-40 w-full">
             <div className="flex justify-between items-center text-[var(--text-black)]">
                 <h2 className="text-lg font-semibold">Portafolio</h2>
-                <div>
-                    <nav className="flex justify-between items-center gap-8 text-sm cursor-pointer">
+                <div className="flex">
+                    <nav className="hidden md:flex font-normal justify-between items-center gap-8 text-sm cursor-pointer">
                         <a id="#inicio" className="hover:text-sky-600">Inicio</a>
+                        <a id="#habilidades" className="hover:text-sky-600">Habilidades</a>
+                        <a id="#proyectos" className="hover:text-sky-600">Proyectos</a>
                         <a id="#acerca" className="hover:text-sky-600">Acerca de</a>
-                        <a id="" className="hover:text-sky-600">Habilidades</a>
-                        <a id="" className="hover:text-sky-600">Proyectos</a>
-                        <a id="" className="hover:text-sky-600">Contacto</a>
                     </nav>
+                    <button></button>
                 </div>
             </div>
         </header>
--- frontend/src/pages/Principal.tsx
+++ frontend/src/pages/Principal.tsx
@@ -1,19 +1,20 @@
 import Acerca from "../components/Acerca";
 import Inicio from "../components/Inicio";
 import Navbar from "../components/Navbar";
+import backgroundImage from "../assets/images/background.png";
 
 export default function Principal () {
     return (
         <>
-        <body className="min-h-screen">
-            <Navbar/>
+        <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <Navbar/>
             <div className="transition-all ease-in-out ">
                 <Inicio/>
             </div>
             <div className="mt-14">
                 <Acerca/>
             </div>
-        </body>
+        </div>
         </>
     )
 }
